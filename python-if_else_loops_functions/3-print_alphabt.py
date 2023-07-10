33dde15d1c8589e2157eea5f739765feeceaebaa
JesseKiguta <j.kiguta@alustudent.com>
2023-07-10 13:38:35 -0700

    pep8

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for letter in range(97,123):
-    if chr(letter) not in ['q','e']:
+for letter in range(97, 123):
+    if chr(letter) not in ['q', 'e']:
         print('{:c}'.format(letter), end='')
