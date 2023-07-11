b15d359b5c352468f71b6f50f66fccf2c0d67c03
JesseKiguta <j.kiguta@alustudent.com>
2023-07-10 17:48:59 -0700

    fizzbuzz fix

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    multiple_of_three = (num % 3) == 0
-    multiple_of_five = (num % 5) == 0
-    multiple_of_both = (num % 15) == 0
     for num in range(1, 101):
-        if multiple_of_three:
+        if (num % 3) == 0:
             print("Fizz", end=" ")
-        elif multiple_of_five:
+        elif (num % 5) == 0:
             print("Buzz", end=" ")
-        elif multiple_of_both:
+        elif (num % 15) == 0:
             print("FizzBuzz", end=" ")
         else:
             print(f"{num}", end=" ")
