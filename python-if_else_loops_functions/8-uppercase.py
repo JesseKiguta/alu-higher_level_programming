9edabed92ca14419bd5c849862a0563c96fe9c9a
JesseKiguta <j.kiguta@alustudent.com>
2023-07-10 17:03:21 -0700

    capsfix

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for char in str:
-        if ord('a') <= ord(char) <= ord('z'):
-            uppercase_char = chr(ord(char) - ord('a') + ord('A'))
-            print(uppercase_char, end="")
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            l = ord(i) - 32
         else:
-            print(char, end="")
-    print()
+            l = ord(i)
+        print("{:c}".format(l), end="")
+    print("")
