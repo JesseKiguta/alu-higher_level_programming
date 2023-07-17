8185d32e7d9f15135c830157b9a2ce72c7ecb43b
JesseKiguta <j.kiguta@alustudent.com>
2023-07-17 13:24:39 -0700

    task3edit1

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -2,6 +2,8 @@
 def common_elements(set_1, set_2):
     common_set = set()
     for elements in set_1:
-        for elements in set_2:
+        if elements in set_2:
             common_set.add(elements)
+        else:
+            return None
     return common_set
