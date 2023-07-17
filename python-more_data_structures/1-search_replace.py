b9a6fc5e49f8253cc112088aeadeeb425c1be2c4
JesseKiguta <j.kiguta@alustudent.com>
2023-07-17 11:56:51 -0700

    task1

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = [replace]
+    if element == search:
+        element = replace
+    else:
+        return element
+    return new_list
