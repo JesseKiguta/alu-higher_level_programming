056da3db936df715e600fb3f483ef555c75b3c3f
JesseKiguta <j.kiguta@alustudent.com>
2023-07-17 11:59:09 -0700

    task1edit1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = [replace]
-    if element == search:
-        element = replace
-    else:
-        return element
+    new_list = [replace if item == search else item for item in my_list]
     return new_list
