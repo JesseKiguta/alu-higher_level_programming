09a649704d995c9942a216e116d016160b69298a
JesseKiguta <j.kiguta@alustudent.com>
2023-07-17 19:26:01 -0700

    pep8,6

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -8,9 +8,9 @@ def roman_to_int(roman_string):
         'L': 50, 'C': 100, 'D': 500, 'M': 1000
     }
     result = 0
-    roman_index = roman_values[roman_string[i]]
 
     for i in range(len(roman_string)):
+        roman_index = roman_values[roman_string[i]]
         if i > 0 and roman_index > roman_values[roman_string[i - 1]]:
             result += roman_index - 2 * roman_values[roman_string[i - 1]]
         else:
