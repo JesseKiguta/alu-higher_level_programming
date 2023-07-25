a91d9efdb1923e23b0e43ffd2e9c2bb0920fabbe
JesseKiguta <j.kiguta@alustudent.com>
2023-07-25 09:31:21 -0700

    task 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+''' defines the size of the square '''
+
+class Square:
+    ''' class defines the size of the square with parameters '''
+    def __init__(self, size=0):
+        ''' initializes attributes '''
+        if size is not int:
+            ''' ensures size is an integer '''
+            raise TypeError("size must be an integer")
+        ''' returns an error message if not an integer '''
+        if size <= 0:
+            ''' ensures size is positive '''
+            raise ValueError("size must be >= 0")
+        ''' returns an error if negative '''
+        else:
+            ''' changes default size to new size '''
+            self.__size = size
