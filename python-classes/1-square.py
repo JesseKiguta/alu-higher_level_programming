9cf4dbf28d35273326c037baf1168b9cc5248581
JesseKiguta <j.kiguta@alustudent.com>
2023-07-25 08:51:48 -0700

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+''' class Square that defines a square with its size '''
+
+
+class Square:
+    def __init__(self, size):
+        ''' initialize class attributes then uses a private instance attribute '''
+        self.__size = size
