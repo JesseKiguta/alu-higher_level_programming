2e2331bb08d9b8e55a4d92c2fc969e1e3ae1a1ab
JesseKiguta <j.kiguta@alustudent.com>
2023-07-25 17:01:55 -0700

    task 2 errorfix

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -11,7 +11,7 @@ class Square:
         the size is not a positive integer """
         if type(size) is not int:
             raise TypeError("size must be an integer")
-        if size <= 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
