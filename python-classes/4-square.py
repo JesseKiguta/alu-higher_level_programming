17152fbb0326f8e4af2dcc7fc5996d8c95147e4a
JesseKiguta <j.kiguta@alustudent.com>
2023-07-25 18:07:13 -0700

    task 4 doc 2

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -19,13 +19,13 @@ class Square:
     @property
     def size(self):
         """ retrieves the __size attribute because
-    it is private """
+        it is private """
         return self.__size
 
     @size.setter
     def size(self, value):
         """ sets the retrieved attribute in
-    a similar way to init """
+        a similar way to init """
         if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:
