6cd6dfa782aa4938257f34d70eea10df964dc882
JesseKiguta <j.kiguta@alustudent.com>
2023-07-27 13:31:17 -0700

    task 6 pep8 2

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -5,6 +5,7 @@
 class Rectangle:
     ''' defines the height and width of a rectangle '''
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         ''' initializes attributes '''
         self.width = width
