88ff721a58681a9f52e4c8be40cd417347b98d5a
JesseKiguta <j.kiguta@alustudent.com>
2023-08-03 12:42:11 -0700

    errorfix 9.0

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,8 +1,8 @@
 -- Creates a table second_table and puts data into it
-CREATE TABLE second_table(
-	id INT DEFAULT NULL,
-	name VARCHAR(256) DEFAULT NULL,
-	score INT DEFAULT NULL
+CREATE TABLE IF NOT EXISTS second_table(
+	id INT,
+	name VARCHAR(256),
+	score INT
 );
 INSERT INTO second_table(id, name, score)
 VALUES (1, 'John' 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
