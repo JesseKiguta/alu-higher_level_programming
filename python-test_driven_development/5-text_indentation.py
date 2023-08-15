51f223b2b257151e8ccab6ebc144f9e0bc63736a
JesseKiguta <j.kiguta@alustudent.com>
2023-08-14 19:34:33 -0700

    ..

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -7,14 +7,32 @@ def text_indentation(text):
     if type(text) is not str:
         raise TypeError("text must be a string")
 
-    special_char_encountered = False
+    new = ""
+    substring = ""
+    temp = [] * 2
+    flag = 0
 
-    for char in text:
-        if char not in ['.', '?', ':']:
-            print(char, end="")
-            special_char_encountered = False
-        else:
-            print(char)
-            special_char_encountered = True
-    if special_char_encountered:
-        print()
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    for i in range(len(text)):
+        if i < len(text) and text[i] == '.' or text[i] == '?'
+           or text[i] == ':':
+            if flag == 0:
+                temp = text.split(text[i], 1)
+                flag = 1
+            else:
+                temp = substring.split(text[i], 1)
+            new += temp[0].lstrip(' ') + text[i]
+            substring = temp[1]
+            print("{:s}".format(new))
+            print()
+            new = ""
+    if flag == 0:
+        text = text.lstrip(' ')
+        text = text.rstrip(' ')
+        print("{:s}".format(text), end="")
+    else:
+        substring = substring.lstrip(' ')
+        substring = substring.rstrip(' ')
+        print("{:s}".format(substring), end="")
