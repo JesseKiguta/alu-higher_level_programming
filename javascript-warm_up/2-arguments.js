be5af0b612c808415e3aeffdd1ed34932fddfaec
JesseKiguta <j.kiguta@alustudent.com>
2023-09-26 13:42:13 -0700

    two

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const { argv } = require('process');
+if (args.length <= 2){
+	console.log('No argument');
+} else if (args.length === 3){
+	console.log('Argument found');
+} else{ 
+	console log ('Arguments found');
+}
