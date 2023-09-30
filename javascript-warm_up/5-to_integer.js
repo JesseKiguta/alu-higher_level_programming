8719e11a37530103bee7d12c0b6efca8675e92cc
JesseKiguta <j.kiguta@alustudent.com>
2023-09-30 10:36:24 -0700

    five fix

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const mynumber = parseInt(Number(process.argv[2]));
 if (isNaN(mynumber)) {
-	console.log('Not a number');
+  console.log('Not a number');
 } else {
-	console.log(`My number: ${mynumber}`);
+  console.log(`My number: ${mynumber}`);
 }
