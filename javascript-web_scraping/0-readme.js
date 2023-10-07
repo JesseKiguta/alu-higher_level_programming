19a8ce8ddb3a76ac6926dc2395308d72dd015538
JesseKiguta <j.kiguta@alustudent.com>
2023-10-07 14:52:35 -0700

    0

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const fs = require('fs');
+const filepath = process.argv[2];
+fs.readFile(filepath, function (err, data) {
+	if (err) {
+		return console.log(err);
+	}
+	console.log(data.toString());
+});
