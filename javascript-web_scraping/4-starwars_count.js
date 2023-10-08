7144c397937f6221d406bc58ca95cdc50639faf1
JesseKiguta <j.kiguta@alustudent.com>
2023-10-07 17:42:21 -0700

    4 edit

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,15 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
-const firstarg = process.argv[2];
-request(firstarg, (error, response, body) => {
-  if (error) {
-    return console.log(error);
-  }
-  const data = JSON.parse(body);
-  let count = 0;
-  for (const movie of data.results) {
-    if (movie.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
-      count++;
-    }
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
